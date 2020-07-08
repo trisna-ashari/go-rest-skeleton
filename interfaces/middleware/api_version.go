180db2823308323a7e615fcdecdefac5e72742b6
Trisna Novi Ashari <trisna.x2@gmail.com>
2020-07-08 23:59:14 +0700

    Added: X-Api-Version injector to response's header

--- interfaces/middleware/api_version.go
+++ interfaces/middleware/api_version.go
@@ -2,19 +2,14 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	"regexp"
 )
 
 func ApiVersion() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
-		accept := c.GetHeader("Accept")
-
-		if accept == "application/vnd.api.v1+json" {
-			c.Set("version", "v1.0")
-		} else {
-			c.Set("version", "v2.0")
-		}
-
+		re, _ := regexp.Compile("v\\d*")
+		version := re.FindStringSubmatch(c.Request.RequestURI)
+		c.Header("X-Api-Version", version[0])
 		c.Next()
 	}
 }
