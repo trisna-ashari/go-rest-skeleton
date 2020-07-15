13dede42b69f926ab098181dee38f393448ebe6c
Trisna Novi Ashari <trisna.x2@gmail.com>
2020-07-15 09:58:14 +0700

    Added: error tracing code

--- infrastructure/exception/error.go
+++ infrastructure/exception/error.go
@@ -43,3 +43,29 @@ var ErrorTextRoleNotFound = errors.New("role.not_found")
 
 // ErrorTextUserNotFound is an error representing user not found in database.
 var ErrorTextUserNotFound = errors.New("user.not_found")
+
+// ErrorCodeIFAUGA001 is an error represent represent given authorization via request headers
+// is not valid.
+// Or does not send authorization.
+const ErrorCodeIFAUGA001 = "IFAUGA001"
+
+// ErrorCodeIFAUGA002 is an error represent request with not supported authentication type.
+const ErrorCodeIFAUGA002 = "IFAUGA002"
+
+// ErrorCodeIFAUGA003 is an error represent decoded Basic Auth does not content
+// pair of username and password.
+const ErrorCodeIFAUGA003 = "IFAUGA003"
+
+// ErrorCodeIFAUGA004 is an error represent username and password from given
+// Basic Auth not registered in the DB.
+const ErrorCodeIFAUGA004 = "IFAUGA004"
+
+// ErrorCodeIFAUGA005 is an error represent invalid JWT Token.
+const ErrorCodeIFAUGA005 = "IFAUGA005"
+
+// ErrorCodeITMIPO001 is an error represent UUID on context is not exists.
+const ErrorCodeITMIPO001 = "ITMIPO001"
+
+// ErrorCodeITMIPO002 is an error represent authenticated user perfom unauthorized request.
+// User does not have permission to perform this request.
+const ErrorCodeITMIPO002 = "ITMIPO002"
