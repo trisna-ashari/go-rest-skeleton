d870e1d184646bea5f95dbdb75b0e458637ba0af
Trisna Novi Ashari <trisna.x2@gmail.com>
2020-09-10 09:31:35 +0700

    Updated: init_factory

--- domain/seeds/init_factory.go
+++ domain/seeds/init_factory.go
@@ -5,29 +5,27 @@ import (
 
 	"github.com/jinzhu/gorm"
 
-	"github.com/bxcodec/faker"
-
 	"github.com/google/uuid"
 )
 
-func initFactory() []Seed {
-	userFaker := entity.UserFaker{}
-	_ = faker.FakeData(&userFaker)
-	user := &entity.User{
+type InitFactory struct {
+	seeders []Seed
+}
+
+var (
+	user = &entity.User{
 		UUID:      uuid.New().String(),
-		FirstName: userFaker.FirstName,
-		LastName:  userFaker.LastName,
-		Email:     "trisna.x2@gmail.com",
-		Phone:     userFaker.Phone,
+		FirstName: "Trisna",
+		LastName:  "Ashari",
+		Email:     "trisna.x22@gmail.com",
+		Phone:     "01234567890",
 		Password:  "123456",
 	}
-
-	role := &entity.Role{
+	role = &entity.Role{
 		UUID: uuid.New().String(),
 		Name: "Super Administrator",
 	}
-
-	permissions := []*entity.Permission{
+	permissions = []*entity.Permission{
 		{UUID: uuid.New().String(), ModuleKey: "user", PermissionKey: "read"},
 		{UUID: uuid.New().String(), ModuleKey: "user", PermissionKey: "create"},
 		{UUID: uuid.New().String(), ModuleKey: "user", PermissionKey: "update"},
@@ -39,69 +37,76 @@ func initFactory() []Seed {
 		{UUID: uuid.New().String(), ModuleKey: "role", PermissionKey: "update"},
 		{UUID: uuid.New().String(), ModuleKey: "role", PermissionKey: "delete"},
 		{UUID: uuid.New().String(), ModuleKey: "role", PermissionKey: "bulk_delete"},
+		{UUID: uuid.New().String(), ModuleKey: "role", PermissionKey: "detail"},
 	}
-
-	rolePermissions := make([]*entity.RolePermission, len(permissions))
-	for i, permission := range permissions {
-		rolePermissions[i] = &entity.RolePermission{
-			UUID:           uuid.New().String(),
-			RoleUUID:       role.UUID,
-			PermissionUUID: permission.UUID,
-		}
-	}
-
-	userRole := &entity.UserRole{
+	userRole = &entity.UserRole{
 		UUID:     uuid.New().String(),
 		UserUUID: user.UUID,
 		RoleUUID: role.UUID,
 	}
+)
 
-	initSeeds := make([]Seed, len(permissions)+len(rolePermissions)+3)
-	initSeeds[0] = Seed{
+func newInitFactory() *InitFactory {
+	return &InitFactory{seeders: make([]Seed, 0)}
+}
+
+func (is *InitFactory) GenerateUserSeeder() *InitFactory {
+	is.seeders = append(is.seeders, Seed{
 		Name: "Create initial user",
 		Run: func(db *gorm.DB) error {
 			_, errDB := createUser(db, user)
 			return errDB
 		},
-	}
+	})
 
-	initSeeds[1] = Seed{
+	return is
+}
+
+func (is *InitFactory) generateRoleSeeder() *InitFactory {
+	is.seeders = append(is.seeders, Seed{
 		Name: "Create initial role",
 		Run: func(db *gorm.DB) error {
 			_, errDB := createRole(db, role)
 			return errDB
 		},
-	}
+	})
 
-	initSeeds[2] = Seed{
-		Name: "Create initial user role",
-		Run: func(db *gorm.DB) error {
-			_, errDB := createUserRole(db, userRole)
-			return errDB
-		},
-	}
+	return is
+}
 
-	for i, p := range permissions {
+func (is *InitFactory) generateRolePermissionsSeeder() *InitFactory {
+	for _, p := range permissions {
 		cp := p
-		initSeeds[i+3] = Seed{
+		is.seeders = append(is.seeders, Seed{
 			Name: "Create initial permission",
 			Run: func(db *gorm.DB) error {
 				_, errDB := createPermission(db, cp)
 				return errDB
 			},
-		}
+		})
 	}
 
-	for i, rp := range rolePermissions {
-		crp := rp
-		initSeeds[i+3+len(permissions)] = Seed{
-			Name: "Create initial role permission",
-			Run: func(db *gorm.DB) error {
-				_, errDB := createRolePermission(db, crp)
-				return errDB
-			},
-		}
-	}
+	return is
+}
 
-	return initSeeds
+func (is *InitFactory) generateUserRoleSeeder() *InitFactory {
+	is.seeders = append(is.seeders, Seed{
+		Name: "Assign initial role to user",
+		Run: func(db *gorm.DB) error {
+			_, errDB := createUserRole(db, userRole)
+			return errDB
+		},
+	})
+
+	return is
+}
+
+func initFactory() []Seed {
+	initialSeeds := newInitFactory()
+	initialSeeds.GenerateUserSeeder()
+	initialSeeds.generateRoleSeeder()
+	initialSeeds.generateRolePermissionsSeeder()
+	initialSeeds.generateUserRoleSeeder()
+
+	return initialSeeds.seeders
 }
