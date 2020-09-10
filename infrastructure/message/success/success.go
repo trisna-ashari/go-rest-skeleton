6bd05fa2a91410edf9fa045408e282ea257879b6
Trisna Novi Ashari <trisna.x2@gmail.com>
2020-09-10 08:59:10 +0700

    Updated: added success.go

---
+++ infrastructure/message/success/success.go
@@ -0,0 +1,25 @@
+package success
+
+const (
+	DevSuccessfullyGenerateRSAKey        = "api.msg.success.dev.successfully_generate_rsa_key"
+	AuthSuccessfullyLogin                = "api.msg.success.auth.successfully_login"
+	AuthSuccessfullyLogout               = "api.msg.success.auth.successfully_logout"
+	AuthSuccessfullyRefreshToken         = "api.msg.success.auth.successfully_refresh_token"
+	AuthSuccessfullyForgotPassword       = "api.msg.success.auth.successfully_send_email_forgot_password"
+	AuthSuccessfullyResetPassword        = "api.msg.success.auth.successfully_reset_password"
+	AuthSuccessfullyGetProfile           = "api.msg.success.auth.successfully_get_profile"
+	RoleSuccessfullyGetRoleList          = "api.msg.success.role.successfully_get_role_list"
+	RoleSuccessfullyGetRoleDetail        = "api.msg.success.role.successfully_get_role_detail"
+	RoleSuccessfullyCreateRole           = "api.msg.success.role.successfully_create_role"
+	RoleSuccessfullyUpdateRole           = "api.msg.success.role.successfully_update_role"
+	RoleSuccessfullyDeleteRole           = "api.msg.success.role.successfully_delete_role"
+	UserSuccessfullyGetUserList          = "api.msg.success.user.successfully_get_user_list"
+	UserSuccessfullyGetUserDetail        = "api.msg.success.user.successfully_get_user_detail"
+	UserSuccessfullyCreateUser           = "api.msg.success.user.successfully_create_user"
+	UserSuccessfullyUpdateUser           = "api.msg.success.user.successfully_update_user"
+	UserSuccessfullyUpdateUserAvatar     = "api.msg.success.user.successfully_update_user_avatar"
+	UserSuccessfullyDeleteUser           = "api.msg.success.user.successfully_delete_user"
+	UserSuccessfullyGetUserPreference    = "api.msg.success.user.successfully_get_user_preference"
+	UserSuccessfullyUpdateUserPreference = "api.msg.success.user.successfully_update_user_preference"
+	UserSuccessfullyResetUserPreference  = "api.msg.success.user.successfully_reset_user_preference"
+)
