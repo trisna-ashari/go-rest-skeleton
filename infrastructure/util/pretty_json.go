c7b99cecd1213bad8f30689c2c475a6d51fbdc76
Trisna Novi Ashari <trisna.x2@gmail.com>
2020-09-10 09:30:07 +0700

    Added: PrettyJSONWithoutIndent method

--- infrastructure/util/pretty_json.go
+++ infrastructure/util/pretty_json.go
@@ -10,7 +10,17 @@ const (
 	tab   = "\t"
 )
 
-func PrettyJSON(data interface{}) string {
+// PrettyJSONWithoutIndent formats the given interface and return json string without indentation and tab.
+func PrettyJSONWithoutIndent(data interface{}) string {
+	buffer := new(bytes.Buffer)
+	encoder := json.NewEncoder(buffer)
+
+	_ = encoder.Encode(data)
+	return buffer.String()
+}
+
+// PrettyJSONWithIndent formats the given interface and return json string with indentation and tab.
+func PrettyJSONWithIndent(data interface{}) string {
 	buffer := new(bytes.Buffer)
 	encoder := json.NewEncoder(buffer)
 	encoder.SetIndent(empty, tab)
