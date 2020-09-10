216cc0b359970d2e45faa953a1f14f250da86cca
Trisna Novi Ashari <trisna.x2@gmail.com>
2020-09-10 09:28:23 +0700

    Updated: role

--- application/role_app.go
+++ application/role_app.go
@@ -9,6 +9,7 @@ type roleApp struct {
 	us repository.RoleRepository
 }
 
+// roleApp implement the RoleAppInterface.
 var _ RoleAppInterface = &roleApp{}
 
 // RoleAppInterface is an interface.
@@ -21,32 +22,32 @@ type RoleAppInterface interface {
 	GetRolePermissions(UUID string) ([]entity.RolePermission, error)
 }
 
-// SaveRole is implementation of method SaveRole.
+// SaveRole is an implementation of method SaveRole.
 func (u *roleApp) SaveRole(Role *entity.Role) (*entity.Role, map[string]string, error) {
 	return u.us.SaveRole(Role)
 }
 
-// UpdateRole is implementation of method UpdateRole.
+// UpdateRole is an implementation of method UpdateRole.
 func (u *roleApp) UpdateRole(UUID string, Role *entity.Role) (*entity.Role, map[string]string, error) {
 	return u.us.UpdateRole(UUID, Role)
 }
 
-// DeleteRole is implementation of method SaveRole.
+// DeleteRole is an implementation of method SaveRole.
 func (u *roleApp) DeleteRole(UUID string) error {
 	return u.us.DeleteRole(UUID)
 }
 
-// GetRole is implementation of method GetRole.
+// GetRole is an implementation of method GetRole.
 func (u *roleApp) GetRole(UUID string) (*entity.Role, error) {
 	return u.us.GetRole(UUID)
 }
 
-// GetRolePermissions is implementation of method GetRolePermissions.
+// GetRolePermissions is an implementation of method GetRolePermissions.
 func (u *roleApp) GetRolePermissions(UUID string) ([]entity.RolePermission, error) {
 	return u.us.GetRolePermissions(UUID)
 }
 
-// GetRoles is implementation of method GetRoles.
+// GetRoles is an implementation of method GetRoles.
 func (u *roleApp) GetRoles(p *repository.Parameters) ([]entity.Role, interface{}, error) {
 	return u.us.GetRoles(p)
 }
--- interfaces/routers/role_router.go
+++ interfaces/routers/role_router.go
@@ -15,9 +15,9 @@ func roleRoutes(e *gin.Engine, r *Router, rg *RouterAuthGateway) {
 
 	v1 := e.Group("/api/v1/external")
 
-	v1.GET("/roles", guard.Authenticate(), guard.Authorize("role_read"), roleV1.GetRoles)
-	v1.POST("/roles", guard.Authenticate(), guard.Authorize("role_create"), roleV1.SaveRole)
-	v1.GET("/roles/:uuid", guard.Authenticate(), guard.Authorize("role_detail"), roleV1.GetRole)
-	v1.PUT("/roles/:uuid", guard.Authenticate(), guard.Authorize("role_update"), roleV1.UpdateRole)
-	v1.DELETE("/roles/:uuid", guard.Authenticate(), guard.Authorize("role_delete"), roleV1.DeleteRole)
+	v1.GET("/roles", guard.Authenticate(), guard.Authorize("read"), roleV1.GetRoles)
+	v1.POST("/roles", guard.Authenticate(), guard.Authorize("create"), roleV1.SaveRole)
+	v1.GET("/roles/:uuid", guard.Authenticate(), guard.Authorize("detail"), roleV1.GetRole)
+	v1.PUT("/roles/:uuid", guard.Authenticate(), guard.Authorize("update"), roleV1.UpdateRole)
+	v1.DELETE("/roles/:uuid", guard.Authenticate(), guard.Authorize("delete"), roleV1.DeleteRole)
 }
