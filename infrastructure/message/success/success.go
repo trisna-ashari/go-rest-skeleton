ac991e3c773fa9c55261ddc90819b77b70d943ca
Trisna Novi Ashari <trisna@privyid.tech>
2020-09-17 09:29:39 +0700

    Grouped: by kind of message

--- infrastructure/message/success/success.go
+++ infrastructure/message/success/success.go
@@ -1,18 +1,31 @@
 package success
 
+// Common success message.
 const (
-	DevSuccessfullyGenerateRSAKey        = "api.msg.success.dev.successfully_generate_rsa_key"
-	AuthSuccessfullyLogin                = "api.msg.success.auth.successfully_login"
-	AuthSuccessfullyLogout               = "api.msg.success.auth.successfully_logout"
-	AuthSuccessfullyRefreshToken         = "api.msg.success.auth.successfully_refresh_token"
-	AuthSuccessfullyForgotPassword       = "api.msg.success.auth.successfully_send_email_forgot_password"
-	AuthSuccessfullyResetPassword        = "api.msg.success.auth.successfully_reset_password"
-	AuthSuccessfullyGetProfile           = "api.msg.success.auth.successfully_get_profile"
-	RoleSuccessfullyGetRoleList          = "api.msg.success.role.successfully_get_role_list"
-	RoleSuccessfullyGetRoleDetail        = "api.msg.success.role.successfully_get_role_detail"
-	RoleSuccessfullyCreateRole           = "api.msg.success.role.successfully_create_role"
-	RoleSuccessfullyUpdateRole           = "api.msg.success.role.successfully_update_role"
-	RoleSuccessfullyDeleteRole           = "api.msg.success.role.successfully_delete_role"
+	DevSuccessfullyGenerateRSAKey = "api.msg.success.dev.successfully_generate_rsa_key"
+)
+
+// Success message for auth.
+const (
+	AuthSuccessfullyLogin          = "api.msg.success.auth.successfully_login"
+	AuthSuccessfullyLogout         = "api.msg.success.auth.successfully_logout"
+	AuthSuccessfullyRefreshToken   = "api.msg.success.auth.successfully_refresh_token"
+	AuthSuccessfullyForgotPassword = "api.msg.success.auth.successfully_send_email_forgot_password"
+	AuthSuccessfullyResetPassword  = "api.msg.success.auth.successfully_reset_password"
+	AuthSuccessfullyGetProfile     = "api.msg.success.auth.successfully_get_profile"
+)
+
+// Success message for role.
+const (
+	RoleSuccessfullyGetRoleList   = "api.msg.success.role.successfully_get_role_list"
+	RoleSuccessfullyGetRoleDetail = "api.msg.success.role.successfully_get_role_detail"
+	RoleSuccessfullyCreateRole    = "api.msg.success.role.successfully_create_role"
+	RoleSuccessfullyUpdateRole    = "api.msg.success.role.successfully_update_role"
+	RoleSuccessfullyDeleteRole    = "api.msg.success.role.successfully_delete_role"
+)
+
+// Success message for user.
+const (
 	UserSuccessfullyGetUserList          = "api.msg.success.user.successfully_get_user_list"
 	UserSuccessfullyGetUserDetail        = "api.msg.success.user.successfully_get_user_detail"
 	UserSuccessfullyCreateUser           = "api.msg.success.user.successfully_create_user"
