b2349cec99b0754cd471f7d5c174b7f45964aaef
Trisna Novi Ashari <trisna@privyid.tech>
2020-09-17 09:29:28 +0700

    Grouped: by kind of error

--- infrastructure/message/exception/error.go
+++ infrastructure/message/exception/error.go
@@ -1,110 +1,98 @@
 package exception
 
-import "errors"
+import (
+	"errors"
+)
 
-// ErrorTextNoRecordInsertedToRedis is an error representing there is no record inserted to redis.
-var ErrorTextNoRecordInsertedToRedis = errors.New("api.msg.error.common.no_record_inserted_to_redis")
+// Common errors.
+var (
+	// ErrorTextNoRecordInsertedToRedis is an error representing there is no record inserted to redis.
+	ErrorTextNoRecordInsertedToRedis = errors.New("api.msg.error.common.no_record_inserted_to_redis")
 
-// ErrorTextInternalServerError is an error representing internal server error.
-var ErrorTextInternalServerError = errors.New("api.msg.error.common.internal_server_error")
+	// ErrorTextInternalServerError is an error representing internal server error.
+	ErrorTextInternalServerError = errors.New("api.msg.error.common.internal_server_error")
 
-// ErrorTextAnErrorOccurred is an error representing an error occurred.
-var ErrorTextAnErrorOccurred = errors.New("api.msg.error.common.an_error_occurred")
+	// ErrorTextAnErrorOccurred is an error representing an error occurred.
+	ErrorTextAnErrorOccurred = errors.New("api.msg.error.common.an_error_occurred")
 
-// ErrorTextUnauthorized is an error representing unauthorized request.
-var ErrorTextUnauthorized = errors.New("api.msg.error.common.unauthorized")
+	// ErrorTextUnauthorized is an error representing unauthorized request.
+	ErrorTextUnauthorized = errors.New("api.msg.error.common.unauthorized")
 
-// ErrorTextForbidden is an error representing forbidden request.
-var ErrorTextForbidden = errors.New("api.msg.error.common.forbidden")
+	// ErrorTextForbidden is an error representing forbidden request.
+	ErrorTextForbidden = errors.New("api.msg.error.common.forbidden")
 
-// ErrorTextBadRequest is an error representing bad request.
-var ErrorTextBadRequest = errors.New("api.msg.error.common.bad_request")
+	// ErrorTextBadRequest is an error representing bad request.
+	ErrorTextBadRequest = errors.New("api.msg.error.common.bad_request")
 
-// ErrorTextUnprocessableEntity is an error representing unprocessable entity.
-var ErrorTextUnprocessableEntity = errors.New("api.msg.error.common.unprocessable_entity")
+	// ErrorTextUnprocessableEntity is an error representing unprocessable entity.
+	ErrorTextUnprocessableEntity = errors.New("api.msg.error.common.unprocessable_entity")
 
-// ErrorTextNotFound is an error representing request not found.
-var ErrorTextNotFound = errors.New("api.msg.error.common.not_found")
+	// ErrorTextNotFound is an error representing request not found.
+	ErrorTextNotFound = errors.New("api.msg.error.common.not_found")
 
-// ErrorTextFileTooLarge is an error representing that received file size too large.
-var ErrorTextFileTooLarge = errors.New("api.msg.error.common.file_too_large")
+	// ErrorTextFileTooLarge is an error representing that received file size too large.
+	ErrorTextFileTooLarge = errors.New("api.msg.error.common.file_too_large")
 
-// ErrorTextInvalidPrivateKey is an error representing invalid private key.
-var ErrorTextInvalidPrivateKey = errors.New("api.msg.error.common.invalid_private_key")
+	// ErrorTextInvalidPrivateKey is an error representing invalid private key.
+	ErrorTextInvalidPrivateKey = errors.New("api.msg.error.common.invalid_private_key")
 
-// ErrorTextInvalidPublicKey is an error representing invalid public key.
-var ErrorTextInvalidPublicKey = errors.New("api.msg.error.common.invalid_public_key")
+	// ErrorTextInvalidPublicKey is an error representing invalid public key.
+	ErrorTextInvalidPublicKey = errors.New("api.msg.error.common.invalid_public_key")
 
-// ErrorTextRefreshTokenIsExpired is an error representing refresh token is expired.
-var ErrorTextRefreshTokenIsExpired = errors.New("api.msg.error.common.refresh_token_expired")
+	// ErrorTextRefreshTokenIsExpired is an error representing refresh token is expired.
+	ErrorTextRefreshTokenIsExpired = errors.New("api.msg.error.common.refresh_token_expired")
 
-// ErrorTextRoleNotFound is an error representing role not found in database.
-var ErrorTextRoleNotFound = errors.New("api.msg.error.role.not_found")
+	// ErrorTextPerPage is an error representing request per page over the limit.
+	ErrorTextPerPage = errors.New("api.msg.error.common.per_page")
+)
 
-// ErrorTextRoleInvalidUUID is an error representing UUID not found in database.
-var ErrorTextRoleInvalidUUID = errors.New("api.msg.error.role.invalid_uuid")
+// Errors for role.
+var (
+	// ErrorTextRoleNotFound is an error representing role not found in database.
+	ErrorTextRoleNotFound = errors.New("api.msg.error.role.not_found")
 
-// ErrorTextStorageCategoryNotFound is an error representing storage_category not found in database.
-var ErrorTextStorageCategoryNotFound = errors.New("storage_category.not_found")
+	// ErrorTextRoleInvalidUUID is an error representing UUID not found in database.
+	ErrorTextRoleInvalidUUID = errors.New("api.msg.error.role.invalid_uuid")
+)
 
-// ErrorTextStorageFileNotFound is an error representing storage_file not found in database.
-var ErrorTextStorageFileNotFound = errors.New("storage_file.not_found")
+// Errors for user.
+var (
+	// ErrorTextUserNotFound is an error representing user not found in database.
+	ErrorTextUserNotFound = errors.New("api.msg.error.user.not_found")
 
-// ErrorTextUserNotFound is an error representing user not found in database.
-var ErrorTextUserNotFound = errors.New("api.msg.error.user.not_found")
+	// ErrorTextUserInvalidUUID is an error representing UUID not found in database.
+	ErrorTextUserInvalidUUID = errors.New("api.msg.error.user.invalid_uuid")
 
-// ErrorTextUserInvalidUUID is an error representing UUID not found in database.
-var ErrorTextUserInvalidUUID = errors.New("api.msg.error.user.invalid_uuid")
+	// ErrorTextUserInvalidPassword is an error representing hashed password not match with stored in database.
+	ErrorTextUserInvalidPassword = errors.New("api.msg.error.user.invalid_password")
 
-// ErrorTextUserInvalidPassword is an error representing hashed password not match with stored in database.
-var ErrorTextUserInvalidPassword = errors.New("api.msg.error.user.invalid_password")
+	// ErrorTextUserInvalidUsernameAndPassword is an error representing hashed password not match with stored in database.
+	ErrorTextUserInvalidUsernameAndPassword = errors.New("api.msg.error.user.invalid_email_and_password")
 
-// ErrorTextUserInvalidUsernameAndPassword is an error representing hashed password not match with stored in database.
-var ErrorTextUserInvalidUsernameAndPassword = errors.New("api.msg.error.user.invalid_email_and_password")
+	// ErrorTextUserEmailNotRegistered is an error representing email already is not exists in database.
+	ErrorTextUserEmailNotRegistered = errors.New("api.msg.error.user.email_not_registered")
 
-// ErrorTextUserEmailNotRegistered is an error representing email already is not exists in database.
-var ErrorTextUserEmailNotRegistered = errors.New("api.msg.error.user.email_not_registered")
+	// ErrorTextUserEmailAlreadyTaken is an error representing email already exists in database.
+	ErrorTextUserEmailAlreadyTaken = errors.New("api.msg.error.user.email_already_taken")
 
-// ErrorTextUserEmailAlreadyTaken is an error representing email already exists in database.
-var ErrorTextUserEmailAlreadyTaken = errors.New("api.msg.error.user.email_already_taken")
+	// ErrorTextUserPreferenceInvalidUUID is an error representing UUID not found in database.
+	ErrorTextUserPreferenceInvalidUUID = errors.New("user_preference.invalid_uuid")
+)
 
-// ErrorTextUserPreferenceInvalidUUID is an error representing UUID not found in database.
-var ErrorTextUserPreferenceInvalidUUID = errors.New("user_preference.invalid_uuid")
+// Errors for storage.
+var (
+	// ErrorTextStorageCategoryNotFound is an error representing storage_category not found in database.
+	ErrorTextStorageCategoryNotFound = errors.New("api.msg.error.storage.category.not_found")
 
-// ErrorTextUploadCannotOpenFile is an error representing uploaded file can not opened by system.
-var ErrorTextUploadCannotOpenFile = errors.New("api.msg.error.common.cannot_open_file")
+	// ErrorTextStorageFileNotFound is an error representing storage_file not found in database.
+	ErrorTextStorageFileNotFound = errors.New("api.msg.error.storage.file.not_found")
 
-// ErrorTextUploadInvalidSize is an error representing uploaded file size is greater than allowed maximum size.
-var ErrorTextUploadInvalidSize = errors.New("api.msg.error.common.upload_invalid_size")
+	// ErrorTextStorageUploadCannotOpenFile is an error representing uploaded file can not opened by system.
+	ErrorTextStorageUploadCannotOpenFile = errors.New("api.msg.error.storage.file.cannot_open_file")
 
-// ErrorTextUploadInvalidFileType is an error representing uploaded file has invalid file type.
-var ErrorTextUploadInvalidFileType = errors.New("api.msg.error.common.upload_invalid_file_type")
+	// ErrorTextStorageUploadInvalidSize is an error representing uploaded file size is greater than allowed maximum size.
+	ErrorTextStorageUploadInvalidSize = errors.New("api.msg.error.storage.file.invalid_file_size")
 
-// ErrorTextPerPage is an error representing request per page over the limit.
-var ErrorTextPerPage = errors.New("api.msg.error.common.per_page")
-
-// ErrorCodeIFAUGA001 is an error represent represent given authorization via request headers
-// is not valid.
-// Or does not send authorization.
-const ErrorCodeIFAUGA001 = "IFAUGA001"
-
-// ErrorCodeIFAUGA002 is an error represent request with not supported authentication type.
-const ErrorCodeIFAUGA002 = "IFAUGA002"
-
-// ErrorCodeIFAUGA003 is an error represent decoded Basic Auth does not content
-// pair of username and password.
-const ErrorCodeIFAUGA003 = "IFAUGA003"
-
-// ErrorCodeIFAUGA004 is an error represent username and password from given
-// Basic Auth not registered in the DB.
-const ErrorCodeIFAUGA004 = "IFAUGA004"
-
-// ErrorCodeIFAUGA005 is an error represent invalid JWT Token.
-const ErrorCodeIFAUGA005 = "IFAUGA005"
-
-// ErrorCodeITMIPO001 is an error represent UUID on context is not exists.
-const ErrorCodeITMIPO001 = "ITMIPO001"
-
-// ErrorCodeITMIPO002 is an error represent authenticated user perfom unauthorized request.
-// User does not have permission to perform this request.
-const ErrorCodeITMIPO002 = "ITMIPO002"
+	// ErrorTextStorageUploadInvalidFileType is an error representing uploaded file has invalid file type.
+	ErrorTextStorageUploadInvalidFileType = errors.New("api.msg.error.storage.file.invalid_file_type")
+)
